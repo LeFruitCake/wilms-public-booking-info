267ed41b5a5f4df05d2038c87762da1816bbc6b5
Jandel Macabecha <jandel.macabecha88@gmail.com>
2025-03-26 15:23:57 +0800

    checked and for deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,54 @@
+"use client"
+import FullCalendarComponent from "./Components/FullCalendar";
+import OtherHousesIcon from '@mui/icons-material/OtherHouses';
+import CallEndIcon from '@mui/icons-material/CallEnd';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LanguageIcon from '@mui/icons-material/Language';
+import { Tooltip, Typography } from "@mui/material";
+import catplying from '../../public/cat_playing.gif'
+import willogo from '../../public/wil_logo.png'
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    return (
+      <div className="flex flex-col w-full min-h-screen justify-center items-center ">
+        <div className="flex flex-col bg-amber-400 w-full p-6 relative">
+          <Image src={willogo.src} alt="logo" width={150} height={150} />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 flex justify-center items-center flex-grow">
+          <FullCalendarComponent />
+        </div>
+        <div className="w-full relative"><Image src={catplying.src} alt="catplaying" width={150} height={150} className=" left-0 top-0" style={{marginBottom:'-0.65em'}}/></div>
+        <div className="w-full m-auto bg-black p-6 flex flex-col md:flex-row lg:flex-row xl:flex-row gap-3 md:gap-24 lg:gap-24 xl:gap-24 justify-center">
+            <div className="flex gap-3">
+              <Tooltip title='Inhouse'><span className="rounded-full p-1"><OtherHousesIcon sx={{color:'white'}} fontSize="medium"/></span></Tooltip>
+              <div className="flex flex-col">
+                <Typography variant="caption" color="silver">Inhouse</Typography>
+                <Typography variant="subtitle1" fontWeight={"bold"} className="text-amber-400">188</Typography>
+              </div>
+            </div>
+            <div className="flex gap-3">
+              <Tooltip title='Inhouse'><span className="rounded-full p-1"><CallEndIcon sx={{color:'white'}} fontSize="medium"/></span></Tooltip>
+              <div className="flex flex-col">
+                <Typography variant="caption" color="silver">Landline</Typography>
+                <Typography variant="subtitle1" fontWeight={"bold"} className="text-amber-400">411-2000-188</Typography>
+              </div>
+            </div>
+            <div className="flex gap-3">
+              <Tooltip title='Inhouse'><span className="rounded-full p-1"><FacebookIcon sx={{color:'white'}} fontSize="medium"/></span></Tooltip>
+              <div className="flex flex-col">
+                <Typography variant="caption" color="silver">Facebook</Typography>
+                <a href="https://www.facebook.com/@CITUWildcatInnovLabs/" target="_blank"><Typography variant="subtitle1" fontWeight={"bold"} className="text-amber-400">CIT-U Wildcat Innovation Labs</Typography></a>
+              </div>
+            </div>
+            <div className="flex gap-3 relative">
+              <Tooltip title='Inhouse'><span className="rounded-full p-1"><LanguageIcon sx={{color:'white'}} fontSize="medium"/></span></Tooltip>
+              <div className="flex flex-col">
+                <Typography variant="caption" color="silver">Website</Typography>
+                <a href="http://wildcatinnovationlabs.com" target="_blank"><Typography variant="subtitle1" fontWeight={"bold"} className="text-amber-400">wildcatinnovationlabs.com</Typography></a>
+              </div>
+            </div>
+        </div>
+      </div>
+    );
 }
